5d13ebcdc14bacf466fb6af103514ce88d3d9d51
neha2124 <nehashukla2124@gmail.com>
2023-02-18 13:59:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const url= "https://ipapi.co/json/"
+const look = document.querySelector("#lookup")
+const ipdisplay = document.querySelector(".ip-display")
+const locationDisplay = document.querySelector(".location-display")
+const geo = document.querySelector(".geo-location")
+const load = document.querySelector(".loading-container")
+const detail = document.querySelector(".data")
+look.addEventListener("click",()=>{
+    
+    fetch(url)
+    
+
+.then((res)=> res.json())
+
+
+.then(data =>{
+    ipdisplay.textContent =`Ip :- ${data.ip}`
+    locationDisplay.textContent=`Location :- ${data.city},${data.region},${data.country_name}`
+    geo.textContent =`Geo Location :- ${data.latitude} Lat, ${data.longitude} Long`
+})
+
+})
+// function fetch ()
